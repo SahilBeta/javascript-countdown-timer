9c25ff629ae6536f26e7bf293e31141d3c4f4af5
SahilBeta <131257680+SahilBeta@users.noreply.github.com>
2023-04-19 16:33:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //Set date to countdown to:
-let countDownFromDate = new Date('Feb 1, 2020 12:00:00').toDateString();
-let countDownDate = new Date('Feb 1, 2020 12:00:00').getTime();
+let countDownFromDate = new Date('May 19, 2023 12:00:00').toDateString();
+let countDownDate = new Date('May 19, 2023 12:00:00').getTime();
 let x = setInterval(function(){
 let now = new Date().getTime();
 //get distance between current date to countdown date
